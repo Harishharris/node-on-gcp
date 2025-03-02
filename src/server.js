03e943f5351e1b42bf5e2d6502a5bc83e8a1b0d0
Harish <harish.16634@gmail.com>
2025-03-02 12:00:47 +0530

    removed stress test

--- src/server.js
+++ src/server.js
@@ -3,16 +3,17 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const leaks = [];
-  setInterval(() => {
-    leaks.push(Buffer.alloc(1000000)); // 1MB allocations every second
-  }, 1000);
-  setInterval(() => {
-    leaks.push(Buffer.alloc(1000000)); // 1MB allocations every second
-  }, 1000);
   console.log(process.memoryUsage());
-
   res.send('Hello from the server');
+});
+
+app.get('/other', (req, res) => {
+  console.log(process.memoryUsage());
+  res.send('Hello from the other route');
+});
+
+app.get('/health', (req, res) => {
+  res.send("I'm healthy");
 });
 
 app.listen(3000, () => {
